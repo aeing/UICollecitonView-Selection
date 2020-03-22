4f130d64e11f2ca06e6f1724d72901f0d1ce4a16
aeing <12443771@qq.com>
2020-03-22 20:57:45 +0800

    project init

---
+++ UICollecitonView-Selection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollecitonView-Selection
+//
+//  Created by zuolin on 2020/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
